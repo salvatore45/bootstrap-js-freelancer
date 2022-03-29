2fe4f14004296a6c6a3982ecf62b311ca626af4c
salvatore <tore0145@gmail.com>
2022-03-29 17:26:55 +0200

    js2

--- main.js
+++ main.js
@@ -6,7 +6,25 @@ var code = document.modulo.code.value;
 var work = document.modulo.work.option[document.modulo.work.selectedIndex].value;
 let codicivalidi=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"]
 console.log("Carico lista codici validi");
-if(work='Backed Development'){
+let trovato=false;
+for(let i=0;i<codicivalidi.length;i++){
+    if(codicivalidi[i] == code){
+        trovato=true;
+        break;
+    }
+}
+if(trovato){
+    if(work='Backed Development'){ 
+        prezzoT=hour*20.5*25/100;
+    }else if(work='Frontend Development'){
+        prezzoT=hour*15.3*25/100;
+    }else if(work="Project Analisys"){
+        perzzoT=hour*33.6*25/100;
+    }else{
+        alert('selezionare che lavoro vuole commissionare');
+    }
+}else{
+    if(work='Backed Development'){ 
     prezzoT=hour*20.5;
 }else if(work='Frontend Development'){
     prezzoT=hour*15.3;
@@ -14,4 +32,5 @@ if(work='Backed Development'){
     perzzoT=hour*33.6;
 }else{
     alert('selezionare che lavoro vuole commissionare');
+}
 }
